08e711bdcd9fa74b234a64dbf1a5edef1da4bbbb
bielzinmatadordeporco <166418764+bielzinmatadordeporco@users.noreply.github.com>
2024-10-22 09:36:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao(
+    'Armas',
+    'O que é um glock??',
+    'Glock é uma série de pistolas semiautomáticas com armação de polímero, com sistema de funcionamento por curto recuo basculante do cano, com culatra no sistema Delayed Blowback.'
+)
+criaCartao(
+    'Medicina',
+    'O que é drogas?',
+    'Droga é toda e qualquer substância, natural ou sintética que, introduzida no organismo modifica suas funções.'
+)
+criaCartao(
+    'Cultura',
+    'O que é capeira?',
+    'A capoeira ou capoeiragem é uma expressão cultural e esporte afro-brasileiro que mistura arte marcial, dança e música, desenvolvida no Brasil por descendentes de escravos africanos.'
+)
+criaCartao(
+    'Educação',
+    'O que é bullying?',
+    'Bullying, também chamado de intimidação sistemática, intimidação vexatória, violência escolar e bulimento, é o uso de força física, ameaça ou coerção para abusar, intimidar ou dominar agressivamente outras pessoas de forma frequente e habitual.'
+)
+criaCartao(
+    'Química',
+    'O que é um átomo?',
+    'Átomo é uma unidade básica de matéria que consiste num núcleo central de carga elétrica positiva envolto por uma nuvem de eletrões de carga negativa')
+
+criaCartao(
+    'Química',
+    'O que causa uma explosão?',
+    'A explosão inicia-se quando um estímulo exterior provoca um aumento de energia cinética no explosivo, levando à quebra das ligações das moléculas afetadas, provocando a sua decomposição e consequente libertação de energia')
